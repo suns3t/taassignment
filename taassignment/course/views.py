d6fc14627c973d534a88adbab3e210e6fd60ade1
William Gonzalez <whg@pdx.edu>
2014-05-05 10:03:38 -0700

    Upload files update

--- taassignment/course/views.py
+++ taassignment/course/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.contrib import messages
 from django.shortcuts import render, render_to_response
 from django.db import IntegrityError
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import decorators
 from taassignment.course.models import Course
@@ -29,80 +30,48 @@ def teacher_view_list(request):
 
 
 
-def request_csv_tas_upload(f):
-    for r in csv.reader(f,delimiter=",",quoting=csv.QUOTE_NONNUMERIC):
-            Oidn = r
-            try:
-                user_exists = User.objects.get(username=format(*Oidn))
-            except User.DoesNotExist:
-                user = User()
-                user.username = format(*Oidn)
-                user.is_ta  = 1
-                user.is_active = 1
-                user.save()
-
-def request_csv_courses_upload(f):
-    for r in csv.reader(f,delimiter=",",quoting=csv.QUOTE_NONNUMERIC):
-        course_no, course_name, Oidn = r # IndexError or PackingError
-        try:
-            course = Course.objects.get(course_no=course_no)
-        except Course.DoesNotExist:
-            course = Course()
-            course.course_no = course_no
-            course.course_name = format(course_name)
-            course.save()
-
-        try:
-            user = User.objects.get(username=format(Oidn))
-        except User.DoesNotExist:
-            user = User()
-            user.username  = format(Oidn)
-            user.is_faculty = 1
-            user.is_active = 1
-            user.save()
-
-        course.faculties.course_id  = course_no
-        course.faculties.add(user)
-        course.save()
-
 # Upload Courses
 def upload_courses(request):
-    url = '/admin/'
     error = None
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if  form.is_valid():
             try:
                 with transaction.atomic():
-                    request_csv_courses_upload(request.FILES['file'])
+                    _request_csv_courses_upload(request.FILES['file'])
+            except (TypeError, ValueError) :
+                error='Wrong file type, number of columns do not mach!'
+                form = UploadFileForm()
             except Exception as e:
                 error= '%s (%s)' % (e.message, type(e))
                 form = UploadFileForm()
-                return render(request,'admin/upload_courses_import.html', {'form': form, "error": error})
-            return HttpResponseRedirect(url)
+            else:
+                return HttpResponseRedirect(reverse("staff-home"))
     else:
         form = UploadFileForm()
-        return render(request,'admin/upload_courses_import.html', {'form': form, "error": error})
+    return render(request,'admin/upload_courses_import.html', {'form': form, "error": error})
 
 
 # Upload Tas
 def upload_tas(request):
-    url = '/admin/'
     error = None
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if  form.is_valid():
             try:
                 with transaction.atomic():
-                    request_csv_tas_upload(request.FILES['file'])
+                    _request_csv_tas_upload(request.FILES['file'])
+            except (TypeError, ValueError) :
+                error='Wrong file type, number of columns do not mach!'
+                form = UploadFileForm()
             except Exception as e:
                 error= '%s (%s)' % (e.message, type(e))
                 form = UploadFileForm()
-                return render(request,'admin/upload_tas_import.html', {'form': form, "error": error})
-            return HttpResponseRedirect(url)
+            else:  
+                return HttpResponseRedirect(reverse("staff-home"))
     else:
         form = UploadFileForm()
-        return render(request,'admin/upload_tas_import.html', {'form': form, "error": error})
+    return render(request,'admin/upload_tas_import.html', {'form': form, "error": error})
 
 
 
@@ -153,4 +122,41 @@ def faculty_view_list(request):
         "courses" : courses,
         "has_courses" : no_of_course,
         })
+
+
+def _request_csv_tas_upload(f):
+    for r in csv.reader(f,delimiter=",",quoting=csv.QUOTE_NONNUMERIC):
+        odin = r[0].strip()
+        try:
+            user_exists = User.objects.get(username=odin)
+        except User.DoesNotExist:
+            user = User()
+            user.username = odin
+            user.is_ta  = 1
+            user.is_active = 1
+            user.save()
+
+def _request_csv_courses_upload(f):
+    for r in csv.reader(f,delimiter=",",quoting=csv.QUOTE_NONNUMERIC):
+        course_no, course_name, odin = [item.strip() for item in r] # IndexError or PackingError
+        try:
+            course = Course.objects.get(course_no=course_no)
+        except Course.DoesNotExist:
+            course = Course()
+            course.course_no = course_no
+            course.course_name = course_name
+            course.save()
+
+        try:
+            user = User.objects.get(username=odin)
+        except User.DoesNotExist:
+            user = User()
+            user.username  = odin
+            user.is_faculty = 1
+            user.is_active = 1
+            user.save()
+
+        course.faculties.course_id  = course_no
+        course.faculties.add(user)
+        course.save()
 
