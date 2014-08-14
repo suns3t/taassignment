05d7f7e272ff44b27d350163cc7a5310de1de1c6
Matt Johnson <mdj2@pdx.edu>
2014-08-14 08:31:02 -0700

    Include .env in wsgi.py

--- taassignment/wsgi.py
+++ taassignment/wsgi.py
@@ -1,14 +1,42 @@
 """
-WSGI config for taassignment project.
+WSGI config for datacommons project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
 """
-
 import os
+import site
+import sys
+
+prev_sys_path = list(sys.path)
+root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(root)
+site.addsitedir(os.path.join(root, ".env/lib/python2.6/site-packages"))
+
+# addsitedir adds its directories at the end, but we want our local stuff
+# to take precedence over system-installed packages.
+# See http://code.google.com/p/modwsgi/issues/detail?id=112
+new_sys_path = []
+for item in list(sys.path):
+  if item not in prev_sys_path:
+    new_sys_path.append(item)
+    sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taassignment.settings")
 
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
